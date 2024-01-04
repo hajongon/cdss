3128e7c3ee49571d87ce67314b197f9b835f03d2
hajongon <hajongon@gmail.com>
2024-01-04 17:58:55 +0900

    left top 조정

--- src/components/cdss/IrukeyElement.js
+++ src/components/cdss/IrukeyElement.js
@@ -1,20 +1,26 @@
 import React, { useEffect } from 'react'
 
+function popupCenter(href, w, h) {
+  const xPos = window.innerWidth / 2 - w / 2 // 가로 가운데 정렬
+  const yPos = window.innerHeight / 2 - h / 2 // 세로 가운데 정렬
+
+  console.log(xPos, yPos)
+
+  window.open(
+    href,
+    'pop_name',
+    `width=${w}, height=${h}, left=${xPos}, top=${yPos}, menubar=yes, status=yes, titlebar=yes, resizable=yes`
+  )
+}
+
 const IrukeyElement = () => {
   useEffect(() => {
     alert(
       '새 창이 열립니다. 팝업이 차단되었을 경우 브라우저 설정에서 팝업을 허용해주세요.'
     )
-    window.open(
-      'http://otid-irukey.kr:9080/rma',
-      '',
-      `width=${width}, height=${height}, left=${left}, top=${top}, location=no, status=no, toolbar=no, scrollbars=no`
-    )
-    const width = window.innerWidth / 2 // 현재 창의 가로 크기의 1/2
-    const height = window.innerHeight / 2 // 현재 창의 세로 크기의 1/2
-    const left = window.innerWidth / 4 // 화면 가로 중앙에 위치
-    const top = window.innerHeight / 4 // 화면 세로 중앙에 위치
+    popupCenter('http://otid-irukey.kr:9080/rma', 800, 600)
   }, [])
+
   return <div>팝업이 차단된 경우, 새 창이 열리지 않을 수 있습니다.</div>
 }
 
